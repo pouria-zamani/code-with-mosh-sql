3d54cad2641b74b93a5a530670b17e2257ea6db9
pouria-zamani <pouriaxz2001@gmail.com>
2024-04-09 21:48:13 +0330

    add all of exercises of section 6.

---
+++ 6-Writing Complex Query/Exercise-6-Subqueries-in-Select-Clause.sql
@@ -0,0 +1,14 @@
+-- Subqueries in the select clause exercise
+
+USE sql_invoicing;
+
+SELECT 
+	client_id,
+    name,
+    (SELECT SUM(invoice_total) 
+		FROM invoices i
+		WHERE client_id = c.client_id) AS total_sales,
+	(SELECT AVG(invoice_total) 
+		FROM invoices) AS invoice_average,
+	(SELECT total_sales invoice_average) AS difference 
+FROM clients c
