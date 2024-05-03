7b448cf68c8c65773a46e08642bd9690ec58a20b
pouria-zamani <pouriaxz2001@gmail.com>
2024-05-03 18:50:41 +0330

    add all of exercises of section 9.

---
+++ 9-Stored Procedures/Exercise-1-Creating-Stored-Procedure.sql
@@ -0,0 +1,15 @@
+-- Create a stored procedure called
+-- get_invoices_with_balance
+-- to return all the invoices with a balance > 0
+
+USE sql_invoicing;
+
+DELIMITER $$
+CREATE PROCEDURE get_invoices_with_balance()
+BEGIN
+	SELECT *
+	FROM invoices
+	WHERE (invoice_total - payment_total) > 0;
+END$$
+
+DELIMITER ;
